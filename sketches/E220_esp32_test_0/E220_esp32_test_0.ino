ef2390706b25913b9916f49f3853bcf1002cf07e
grungeee <sharapanikita@gmail.com>
2024-07-19 18:51:32 +0200

    trying to figure out the lib step by step

--- sketches/E220_esp32_test_0/E220_esp32_test_0.ino
+++ sketches/E220_esp32_test_0/E220_esp32_test_0.ino
@@ -1,39 +1,46 @@
-#include <Arduino.h>
-#include <LoRa_E220.h>
-#include <HardwareSerial.h>
+// |==============================< LIBRARIES >===============================|
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#include <Arduino.h> // What do I need this for again?
 
-// |=============< LED RING INCLUDE >==============|
+// |=============< LED RING PRESETS >==============|
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
 #define LED_RING_PIN 21
-#define LEDS_NUM 16
+#define LEDS_NUM 16 // number of LEDs on the ring duh
 
+Adafruit_NeoPixel pixels(LEDS_NUM, LED_RING_PIN, NEO_GRB + NEO_KHZ800);
+
+
+// --------[ Fancy LED Colors Function ]--------
 int red[3] = {255, 0, 0};
 int green[3] = {0, 255, 0};
 int blue[3] = {0, 0, 255};
 
-Adafruit_NeoPixel pixels(LEDS_NUM, LED_RING_PIN, NEO_GRB + NEO_KHZ800);
 
+//led_bit, color, wait
 void LEDR_COLOR(int ledIndex, int color[3], int delayTime) {
   pixels.clear();
   pixels.setPixelColor(ledIndex, pixels.Color(color[0], color[1], color[2]));
   pixels.show();
   delay(delayTime);
 }
-// |=============< LED RING INCLUDE >==============|
 
-HardwareSerial MySerial(2);
-// |===================< E220 >==================|
+// |======================< E220 PRESETS >======================|
+#include <LoRa_E220.h>
+#include <HardwareSerial.h>
+
+HardwareSerial Serial2(2); // Create an instance of HardwareSerial for UART2
+
 #define TX_PIN 17
 #define RX_PIN 16
 #define AUX_PIN 4
 #define M0_PIN 2
 #define M1_PIN 15
 
-LoRa_E220 e220ttl(TX_PIN, RX_PIN, &MySerial, AUX_PIN, UART_BPS_RATE_9600, SERIAL_8N1);
+LoRa_E220 e220ttl(TX_PIN, RX_PIN, &Serial2, AUX_PIN, UART_BPS_RATE_9600, SERIAL_8N1);
 
 void configureChannel() {
     ResponseStructContainer c;
@@ -44,12 +51,22 @@ void configureChannel() {
 
     e220ttl.setConfiguration(configuration, WRITE_CFG_PWR_DWN_SAVE);
 }
-// |=============================================|
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// |==============================< SETUP >===============================|
+//  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void setup() {
-  // Initialize Serial
-  Serial.begin(9600);
-  while (!Serial);
+  // ----------- [ Standard Setup ] -----------
+  Serial.begin(9600); // Initialize Serial
+  while (!Serial); //This line waits for the serial port to be ready before continuing with the rest of the program.
+
+
+  // |=============<  E220 SETUP >==============|
+ // Initialize Serial2 with specific TX and RX pins
+  Serial2.begin(9600, SERIAL_8N1, 17, 16); // TX = GPIO17, RX = GPIO16
 
   // Initialize E220 module
   e220ttl.begin();
@@ -78,6 +95,8 @@ void setup() {
   // |=============================================|
 }
 
+
+// |====================================< LOOP >=========================================|
 void loop() {
   LEDR_COLOR(8, blue, 500);
 
